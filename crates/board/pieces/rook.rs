b98115bf1ec8f952bbcbaa011a469d9395f66acb
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-05-14 23:35:00 -0300

    I hate everything this while loop stands for
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- crates/board/pieces/rook.rs
+++ crates/board/pieces/rook.rs
@@ -74,12 +74,12 @@ impl Moveset for Rook {
             {
                 let mut temp;
                 temp = self.get_position();
-                loop {
+                while temp.x != direction.into() || temp == king.get_position() {
                     temp.x += direction.into();
-                    if teammates.contains(&temp) || opponents.contains(&temp) {
+                    if temp != king.get_position() && teammates.contains(&temp)
+                        || opponents.contains(&temp)
+                    {
                         break 'castle Vec::new();
-                    } else if temp == king.get_position() {
-                        break;
                     }
                 }
             };
@@ -106,7 +106,7 @@ impl Moveset for Rook {
             .0
             .into_iter()
             // You can't move to a position where teammates are standing
-            .filter(|possible_position| !teammates.contains(possible_position))
+            // .filter(|possible_position| !teammates.contains(possible_position))
             .map(|possible_position| {
                 // If you move to a Possition with an opponent, it has a side effect
                 if opponents.contains(&possible_position) {
